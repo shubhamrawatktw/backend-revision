2b10251cb582aa81dded703de1cd4a010661502a
shubham <shubham@itday.in>
2023-07-23 13:06:33 +0530

    manual auth done

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -10,15 +10,42 @@ const createUser = async (req, res) => {
     }
 
     const user = await User.findOne({ email: email.trim() });
-     if (user) {
-         return res.json({msg:"User already exist"})
-     }
+    if (user) {
+      return res.json({ msg: "User already exist" });
+    }
 
-   const userCreated =  await User.create({email,name,password})
-   return res.status(200).json({msg:"User created successfully",data:userCreated})
+    const userCreated = await User.create({ email, name, password });
+    return res
+      .status(200)
+      .json({ msg: "User created successfully", data: userCreated });
+  } catch (error) {
+    console.log(error, "error");
+  }
+};
 
+const signIn = async (req, res) => {
+  console.log(req.body);
 
+  try {
+    const email = req.body.email.trim();
+    const password = req.body.password.trim();
 
+    if (!email || !password) {
+      return res.json({ msg: "invalid credentials" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ msg: "User not found" });
+    }
+
+    if (user.password !== password) {
+      return res.json({ msg: "Invalid credentials - worong password" });
+    }
+
+    res.cookie("user_id", user.id);
+    return res.status(200).json({ msg: "session created", data: user });
   } catch (error) {
     console.log(error, "error");
   }
@@ -26,4 +53,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
   createUser,
+  signIn,
 };
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express")
 const userRouter = express.Router()
 const userController = require("../controller/user_controller")
 userRouter.post("/create",userController.createUser)
+userRouter.post("/sign-in",userController.signIn)
 
 
 
