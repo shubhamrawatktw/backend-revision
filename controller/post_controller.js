c54edcb72b5ef01d5ae702c340bb4b16bd4c2481
shubham <shubham@itday.in>
2023-07-23 16:13:06 +0530

    populate other data & add reference in mongoose

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,18 +1,42 @@
 const Post = require("../models/post");
 
+const getAllPost = async (req, res) => {
+  try {
+    const posts = await Post.find({}).populate({
+        path:"user",
+        select:{
+            name:true,
+            email:true
+        }
+    }).exec();
+    if (!posts) return res.json({ msg: "No Post Found" });
+
+    return res.status(200).json({
+      msg: "Posts fetched successfully",
+      data: posts,
+    });
+  } catch (error) {
+    console.log(error, "error while getting posts");
+  }
+};
+
 const createPost = async (req, res) => {
   try {
     const { content } = req.body;
-    if (!content.trim()) {
+    if (!content) {
       return res.status(500).json({ msg: "Content req" });
     }
-   const post = await Post.create({ content: content.trim(), user:req.user._id });
-   return res.json({msg:"Post created",data:post})
+    const post = await Post.create({
+      content: content.trim(),
+      user: req.user._id,
+    });
+    return res.json({ msg: "Post created", data: post });
   } catch (error) {
-    console.log(error,"error while creating post ")
+    console.log(error, "error while creating post ");
   }
 };
 
 module.exports = {
   createPost,
+  getAllPost,
 };
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const express = require("express")
 const postRouter = express.Router()
 const postController = require("../controller/post_controller")
 
+postRouter.get("/all",postController.getAllPost)
 postRouter.post("/create",postController.createPost)
 
 module.exports = postRouter
